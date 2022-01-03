2c1a23017fd5486c2daa3c4e6b759ef49ca57aca
godambevaibhav <godambevaibhav5@gmail.com>
2022-01-03 21:41:52 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { TemplateDrivenFormsComponent } from './components/template-driven-forms/template-driven-forms.component';
+import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'reactive-forms', component: ReactiveFormsComponent},
+  {path: 'template-driven-forms', component: TemplateDrivenFormsComponent},
+  {path: '', redirectTo: '/reactive-forms', pathMatch:  'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TemplateDrivenFormsComponent } from './components/template-driven-forms/template-driven-forms.component';
+import { ReactiveFormsComponent } from './components/reactive-forms/reactive-forms.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormsComponent,
+    ReactiveFormsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
